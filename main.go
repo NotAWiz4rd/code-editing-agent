7752b1919b740c1820d459537c6a5b487c9e4b1b
notawiz4rd <github@notawiz4rd.com>
2025-04-20 23:59:45 +0200

    add ListFilesTool, move tools to their own files

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 		return scanner.Text(), true
 	}
 
-	tools := []ToolDefinition{ReadFileDefinition}
+	tools := []ToolDefinition{ReadFileDefinition, ListFilesDefinition}
 	agent := NewAgent(&client, getUserMessage, tools)
 	err := agent.Run(context.TODO())
 	if err != nil {
@@ -141,34 +141,6 @@ type ToolDefinition struct {
 	Description string                         `json:"description"`
 	InputSchema anthropic.ToolInputSchemaParam `json:"input_schema"`
 	Function    func(input json.RawMessage) (string, error)
-}
-
-var ReadFileDefinition = ToolDefinition{
-	Name:        "read_file",
-	Description: "Read the contents of a given relative file path. Use this when you want to see what's inside a file. Do not use this with directory names.",
-	InputSchema: ReadFileInputSchema,
-	Function:    ReadFile,
-}
-
-type ReadFileInput struct {
-	Path string `json:"path" jsonschema_description:"The relative path of a file in the working directory."`
-}
-
-var ReadFileInputSchema = GenerateSchema[ReadFileInput]()
-
-func ReadFile(input json.RawMessage) (string, error) {
-	readFileInput := ReadFileInput{}
-	err := json.Unmarshal(input, &readFileInput)
-	if err != nil {
-		panic(err)
-	}
-
-	content, err := os.ReadFile(readFileInput.Path)
-	if err != nil {
-		return "", err
-	}
-
-	return string(content), nil
 }
 
 func GenerateSchema[T any]() anthropic.ToolInputSchemaParam {
